a0e0bec2e9c1aff0c01c0a375491ce6b1ca516df
Torsten Spindler <spindler@Jaguar.fritz.box>
2016-05-13 09:56:17 +0200

    Use argparse, cleanup prints

--- python/changeDbVersionForGroup.py
+++ python/changeDbVersionForGroup.py
@@ -7,18 +7,34 @@ import requests
 from requests.auth import HTTPDigestAuth
 import json
 import sys
+import argparse
 
 
 ### command line
 
-groupname = sys.argv[1]
-newversion = sys.argv[2]
+parser = argparse.ArgumentParser(description='Change database version via MongoDB Ops Manager.')
+parser.add_argument('groupname', help='The Ops Manager group name to update', default='Demo')
+parser.add_argument('newversion', help='The new version of MongoDB to be used', default='3.2.6')
+parser.add_argument('-u', '--user', help='Ops Manager user', default='Admin@localhost.com')
+parser.add_argument('--key', help='Ops Manager key for user', default='52b84900-81f5-4511-8f73-8716a4624842')
+parser.add_argument('--baseurl', help='Ops Manager endpoint', default='http://opsmgr.vagrant.dev:8080/')
+
+args = parser.parse_args()
+
+groupname = args.groupname
+newversion = args.newversion
 
 ### REST request parameters
 
-user = 'Admin@localhost.com'
-passwd = '52b84900-81f5-4511-8f73-8716a4624842'
-baseurl = 'http://opsmgr.vagrant.dev:8080/'
+# First test settings
+# user = 'Admin@localhost.com'
+# passwd = '52b84900-81f5-4511-8f73-8716a4624842'
+# baseurl = 'http://opsmgr.vagrant.dev:8080/'
+
+user = args.user
+passwd = args.key
+baseurl = args.baseurl
+
 
 ### Get the groups
 url = baseurl + 'api/public/v1.0/groups'
@@ -54,14 +70,14 @@ response = requests.get(url, auth=HTTPDigestAuth(user, passwd))
 
 automationConfig_json = json.loads(response.text)
 for process_json in automationConfig_json['processes']:
-  pprint(process_json['version'])
-  pprint("Setting version to " + newversion)
+  # pprint(process_json['version'])
+  print("Setting version to " + newversion)
   process_json['version'] = newversion
 
 # pprint(automationConfig_json)
 headers = {'content-type': 'application/json'}
 response = requests.put(url, data = json.dumps(automationConfig_json), auth=HTTPDigestAuth(user, passwd), headers=headers)
 
-pprint(response.text)
+# pprint(response.text)
 
-pprint("Hopefully deployed new version")
+print("Deploying new version, check Ops Manager for activity")
